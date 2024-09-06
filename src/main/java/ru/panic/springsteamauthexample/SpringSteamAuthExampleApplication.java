547b241f707f95ed6a5243f6d8937b21e1d706b8
Andrey <andrey1010102008@gmail.com>
2024-09-06 20:26:40 +0300

    initiation

---
+++ src/main/java/ru/panic/springsteamauthexample/SpringSteamAuthExampleApplication.java
@@ -0,0 +1,13 @@
+package ru.panic.springsteamauthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSteamAuthExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSteamAuthExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/panic/springsteamauthexample/SpringSteamAuthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.panic.springsteamauthexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSteamAuthExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
